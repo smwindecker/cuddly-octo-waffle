65c7d5182a508e8da9bc63a7dd42ed8b2b5a7efe
Saras Windecker <sm.windecker@unimelb.edu.au>
2019-11-08 16:14:12 +1100

    add read data and make plot script

--- analysis.R
+++ analysis.R
@@ -1,9 +1,16 @@
 # Analysis script for my awesome project
 
 # read data
+# apples <- data.frame(apple_id = as.factor(1:1000),
+#                      weight = rnorm(1000, 5, 1))
+# write.csv(apples, 'data/apples_data.csv', row.names = FALSE)
+
+apples <- read.csv('data/apples_data.csv')
 
 # process data
+apples$log_weight <- log(apples$weight)
 
 # graphing
-
-# print image 
+png('figs/hist_apple_weight.png')
+hist(apples$weight)
+dev.off()
